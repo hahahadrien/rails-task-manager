5b6f75685ff703410b3b94e59f7728eff9aa8986
hadrien le bret <hadrien.lebret@gmail.com>
2020-11-10 16:15:28 +0100

    Task Manager

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,24 +1,47 @@
 class TasksController < ApplicationController
 
-  def list # As a user, I can list tasks
-    @tasks = Task.all
+  def index # As a user, I can list tasks
+    @tasks = Task.all # ici c'est l'instance de classe Task generee par le Model
   end
 
-  def view # As a user, I can view the details of a task
+  def show # As a user, I can view the details of a task
+    # raise
+    @task = Task.find(params[:id])
   end
 
   def new # As a user, I can add a new task
+     @task = Task.new
   end
 
   def create # As a user, I can add + save a new task
+    # strong params
+    @task = Task.new(task_params) # strong params
+    @task.save
+    # redirect_to "restaurants/#{@restaurant.id}"
+    redirect_to task_path(@task) # C'est comme un link to => vers le prefix que j'ai dans mes rails routes (terminal)
   end
 
-  def edit  # As a user, I can edit a task (mark as completed / update title & details)
+  def edit # As a user, I can edit a task (mark as completed / update title & details)
+    @task = Task.find(params[:id])
   end
 
   def update
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+    redirect_to task_path(@task)
   end
 
-  def remove # As a user, I can remove a task
+  def delete # As a user, I can remove a task
+    @task = Task.find(params[:id])
+    @task.destroy
+    # no need for app/views/restaurants/destroy.html.erb
+    redirect_to tasks_path
+  end
+
+  private
+
+  def task_params # Definition des strong params, auxquels on peut avoir acces (hors security)
+    # paramis.require (:nom du model).permit(:nom des params qu'on veut)
+    params.require(:task).permit(:title, :details, :completed)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,25 +3,30 @@ Rails.application.routes.draw do
 
   # Verb 'PATH', to: "CONTROLLER_name#METHOD_name"
 
+  # (3) As a user, I can list tasks
   # get 'restaurants', to: "restaurants#index"
   # # -> /restaurants -> call the method index on the controlle Restaurans
-  get 'tasks', to: 'tasks#list' # list tasks
+  get 'tasks', to: 'tasks#index' # list tasks
 
+  # (5) As a user, I can add a new task
   # post 'restaurants', to: "restaurants#create"
   post 'tasks', to: 'tasks#create' # add a new task
   # get 'restaurants/new', to: "restaurants#new"
   get 'tasks/new', to: 'tasks#new' # add a new task
 
+  # (6) As a user, I can edit a task
   # get 'restaurants/:id/edit', to: "restaurants#edit", as: :edit_restaurant
-  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task # edit a task (mark as completed / update title & details)
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task # allows to put a prefix in rails routes
   # patch 'restaurants/:id', to: "restaurants#update"
   patch 'tasks/:id', to: 'tasks#update' # update a task (mark as completed / update title & details)
 
+  # (4) As a user, I can view the details of a task
   # get 'restaurants/:id', to: "restaurants#show", as: :restaurant
   # # /restaurants/43 -> call the show method on RestaurantsContr...
   # # and give the params {id: 43}
-  get 'tasks/:id', to: 'tasks#view', as: :task # view the details of a task
+  get 'tasks/:id', to: 'tasks#show', as: :task # view the details of a task
 
+  # (7) As a user, I can remove a task
   # delete 'restaurants/:id', to: "restaurants#destroy"
-  get 'tasks/:id', to: 'tasks#remove' # remove a task
+  delete 'tasks/:id', to: 'tasks#delete' # remove a task
 end
